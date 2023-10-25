e59fc949ce2e5602fd8de5dc309cc6bf9f49b405
FaliciaOng <ongfali@gmail.com>
2023-10-25 16:24:47 +0800

    implmented multithreading

--- web_scrap_eg.py
+++ web_scrap_eg.py
@@ -1,23 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+from multiprocessing import Process
 import time
+from colorama import Fore
 # pip install lxml
 
-print("Put skill not familiar with")
-unfamiliar_skill = input('>')
 
-print(f'filtering out {unfamiliar_skill}')
 
-def find_jobs():
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+def find_jobs(url, post_name, color):
+    unfamiliar_skill = 'css'
+    html_text = requests.get(url).text
     # print(html_text)
     soup = BeautifulSoup(html_text,'lxml')
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
     # print(job)
-
+    
     for index,job in enumerate(jobs):
         publish_date = job.find('span', class_="sim-posted").span.text
         # print(publish_date)
+        
 
         if 'few' in publish_date:
             company_name = job.find('h3',class_ = 'joblist-comp-name').text.replace(" ", "")
@@ -28,18 +29,35 @@ def find_jobs():
             more_info = job.header.h2.a['href']
             if unfamiliar_skill not in skills:
 
-                with open(f'posts/{index}.txt', 'w') as f:
+                with open(f'{post_name}/{index}.txt', 'w') as f:
                     f.write(f'Company Name: {company_name.strip()} \n')
                     f.write(f'Req skills: {skills.strip()} \n')
                     f.write(f"More info: {more_info}\n")
-                print(f'File svae: {index}.txt')
+                print(color+f'File svae: {index}.txt')
+            time.sleep(0.5)
                     
 
 
 if __name__ == '__main__':
-    while True:
-        find_jobs()
-        time_wait = 10
-        print(f'waiting for for {time_wait} minutes..')
-        time.sleep(time_wait*60)
+    # print("Put skill not familiar with")
+    # unfamiliar_skill = input('>')
+
+    # print(f'filtering out {unfamiliar_skill}')
+    process1 = Process(target=find_jobs, args=('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=', 'posts', Fore.RED))
+    process2 = Process(target=find_jobs,args=('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=java&txtLocation=', 'posts_two',Fore.YELLOW))
+    # process3 = Process(target=find_jobs,args=('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=java&txtLocation=', 'posts_two',Fore.))
+    process2.start()
+    # process3.start()
+    process1.start()
+    process1.join()
+    process2.join()
+    # process3.join()
+    print("process ended")
+
+
+    # while True:
+        
+    #     time_wait = 10
+    #     # print(f'waiting for for {time_wait} minutes..')
+    #     # time.sleep(time_wait*60)
 
